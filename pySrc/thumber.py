156713f030ae804e7d872d71f1c8e2f83ebcaa9f
sonofoven <novesen@calpoly.edu>
2025-12-11 21:11:47 -0800

    Made code compatible w/ AWS

--- pySrc/thumber.py
+++ pySrc/thumber.py
@@ -1,14 +1,61 @@
 from PIL import Image
-import sys
+from io import BytesIO
+import json
+import boto3
 import os
 
+destBucket = "novesen-s3-thumb-gen-output"
+
 size = (128, 128)
-infile = sys.argv[1]
+s3Client = boto3.client('s3')
 
-outfile = os.path.splitext(infile)[0] + "_thumb.jpg"
+def lambda_handler(event, context):
 
-with Image.open(infile) as im:
-    im.thumbnail(size)
-    im.save(outfile, "JPEG")
+    try:
+        # Get trigger info
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = event['Records'][0]['s3']['object']['key']
+    except Exception as e:
+        return {'statusCode': 400,
+                'body': json.dumps(f'Invalid S3 trigger event. {e}')}
 
-print(f"Saved {outfile}")
+    try:
+        # Grab new object and get its bytes
+        response = s3Client.get_object(Bucket=bucket, Key=key)
+        imageBytes = response['Body'].read()
+    except Exception as e:
+        return {'statusCode': 400,
+                'body': json.dumps(f'Could not grab image bytes. {e}')}
+
+    try:
+        # Make open up a byte buffer and modify the bytes
+        image = Image.open(BytesIO(imageBytes))
+        image.thumbnail(size)
+
+        # Save the image to a new buffer
+        buffer = BytesIO()
+        image.save(buffer, "JPEG")
+        buffer.seek(0)
+    except Exception as e:
+        return {'statusCode': 400,
+                'body': json.dumps(f'Failed to process image. {e}')}
+
+        # Create output name
+        destKey = os.path.splitext(key)[0] + "_thumb.jpg"
+
+    try:
+        # Save converted buffer to the s3 bucket
+        s3Client.put_object(
+            Bucket=destBucket,
+            Key=destKey,
+            Body=buffer,
+            ContentType="image/jpeg"
+        )
+    except Exception as e:
+        return {'statusCode': 400,
+                'body': json.dumps(f'Failed to put object. {e}')}
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('File successfully thumbnailed')
+    }
