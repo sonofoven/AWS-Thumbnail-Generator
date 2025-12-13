02c3dd3701d8ef02d97f49f7e7e7057a6036f041
sonofoven <novesen@calpoly.edu>
2025-12-12 16:42:21 -0800

    Modified deployment strategy to docker container

--- pySrc/lambda_function.py
+++ pySrc/lambda_function.py
@@ -9,13 +9,15 @@ destBucket = "novesen-s3-thumb-gen-output"
 size = (128, 128)
 s3Client = boto3.client('s3')
 
-def lambda_handler(event, context):
+
+def handler(event, context):
 
     try:
         # Get trigger info
         bucket = event['Records'][0]['s3']['bucket']['name']
         key = event['Records'][0]['s3']['object']['key']
     except Exception as e:
+        printf('Invalid S3 trigger event. {e}')
         return {'statusCode': 400,
                 'body': json.dumps(f'Invalid S3 trigger event. {e}')}
 
@@ -25,6 +27,7 @@ def lambda_handler(event, context):
         imageBytes = response['Body'].read()
     except Exception as e:
         return {'statusCode': 400,
+                print(f'Could not grab image bytes. {e}')
                 'body': json.dumps(f'Could not grab image bytes. {e}')}
 
     try:
@@ -37,6 +40,7 @@ def lambda_handler(event, context):
         image.save(buffer, "JPEG")
         buffer.seek(0)
     except Exception as e:
+        print(f'Failed to process image. {e}')
         return {'statusCode': 400,
                 'body': json.dumps(f'Failed to process image. {e}')}
 
@@ -52,9 +56,11 @@ def lambda_handler(event, context):
             ContentType="image/jpeg"
         )
     except Exception as e:
+        print(f'Failed to put object. {e}')
         return {'statusCode': 400,
                 'body': json.dumps(f'Failed to put object. {e}')}
 
+    print('File successfully thumbnailed')
     return {
         'statusCode': 200,
         'body': json.dumps('File successfully thumbnailed')
